6075eae1eec33724d3e6fea9791045e212ae4124
xiagb <x.yuanxin@qq.com>
2015-10-08 11:28:47 +0800

    slave分支修改2

--- src/com/xgb/temp/Test.java
+++ src/com/xgb/temp/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("您好，世界1！");
 		System.out.println("您好，世界2！");
 		System.out.println("您好，世界3 -- slave！");
+		System.out.println("您好，世界4 -- slave！");
 	}
 	
 }
