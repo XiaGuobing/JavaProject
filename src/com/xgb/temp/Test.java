5ffb0021799c2e07648eb88b3d4f160a0064c6d8
xiagb <x.yuanxin@qq.com>
2015-10-08 10:58:31 +0800

    修改了内容1

--- src/com/xgb/temp/Test.java
+++ src/com/xgb/temp/Test.java
@@ -3,7 +3,8 @@ package com.xgb.temp;
 public class Test {
 	
 	public static void main(String[] args){
-		System.out.println("您好，世界！");
+		System.out.println("您好，世界1！");
+		System.out.println("您好，世界2！");
 	}
 	
 }
