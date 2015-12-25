1c032ced7d5b8599b03908723eafc7544ed83995
xiagb <x.yuanxin@qq.com>
2015-12-25 15:05:58 +0800

    Development分支修改2

--- src/com/xgb/temp/Test.java
+++ src/com/xgb/temp/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("初始化");
 		System.out.println("Development分支修改1");
+		System.out.println("Development分支修改2");
 	}
 	
 }
