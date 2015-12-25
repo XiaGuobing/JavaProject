4915f7c6a4252a376f3d4b934726fac8ed2cd73c
xiagb <x.yuanxin@qq.com>
2015-12-25 14:49:44 +0800

    初始化提交

--- src/com/xgb/temp/Test.java
+++ src/com/xgb/temp/Test.java
@@ -3,9 +3,7 @@ package com.xgb.temp;
 public class Test {
 	
 	public static void main(String[] args){
-		System.out.println("您好，世界1！");
-		System.out.println("您好，世界2！");
-		System.out.println("您好，世界3 -- slave！");
+		System.out.println("初始化");
 	}
 	
 }
