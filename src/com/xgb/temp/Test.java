00f285b2b7598507ddd26e2240d8056234d9d280
xiagb <x.yuanxin@qq.com>
2016-03-30 18:08:57 +0800

    commit Test.java

--- src/com/xgb/temp/Test.java
+++ src/com/xgb/temp/Test.java
@@ -6,6 +6,9 @@ public class Test {
 		System.out.println("初始化");
 		System.out.println("Development分支修改1");
 		System.out.println("Development分支修改2");
+		
+		System.out.println("Development分支修改3");
+		System.out.println("Development分支修改4");
 	}
 	
 }
